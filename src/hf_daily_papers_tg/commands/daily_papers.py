12757295254b89e1f2cbba8c4ff76ae8ac50992d
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-11-25 11:33:43 +0300

    add rate limiting

--- src/hf_daily_papers_tg/commands/daily_papers.py
+++ src/hf_daily_papers_tg/commands/daily_papers.py
@@ -1,9 +1,10 @@
 import asyncio
 import datetime
 import logging
+from typing import Any
 
-from telegram import Bot
-from telegram.constants import ParseMode
+from telegram.constants import FloodLimit, ParseMode
+from telegram.ext import AIORateLimiter, ExtBot
 
 from hf_daily_papers_tg.commands.error_message import send_error_message
 from hf_daily_papers_tg.parsers.papers import get_papers
@@ -16,7 +17,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-async def send_daily_papers_update(bot: Bot, settings: Settings) -> None:
+async def send_daily_papers_update(bot: ExtBot[Any], settings: Settings) -> None:
     """Send daily papers update to the channel."""
     await bot.send_message(
         chat_id=settings.tg.admin_user_id,
@@ -34,11 +35,11 @@ async def send_daily_papers_update(bot: Bot, settings: Settings) -> None:
                 text=paper.format(),
                 parse_mode=ParseMode.HTML,
             )
-            await asyncio.sleep(0.05)
-        except Exception:
+            await asyncio.sleep(0.05)  # additional delay to avoid hitting rate limits
+        except Exception as e:
             msg = f"Failed to send papers update. On paper {paper.paper.id}"
             logger.exception(msg)
-            await send_error_message(bot, settings.tg.admin_user_id, Exception(msg))
+            await send_error_message(bot, settings.tg.admin_user_id, e, msg)
 
     logger.info("Finished daily papers update")
     await bot.send_message(
@@ -48,7 +49,14 @@ async def send_daily_papers_update(bot: Bot, settings: Settings) -> None:
 
 
 async def main(settings: Settings) -> None:
-    async with Bot(settings.tg.bot_token) as bot:
+    async with ExtBot(
+        settings.tg.bot_token,
+        rate_limiter=AIORateLimiter(
+            # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Avoiding-flood-limits
+            overall_max_rate=FloodLimit.MESSAGES_PER_MINUTE_PER_GROUP - 1,
+            max_retries=2,
+        ),
+    ) as bot:
         try:
             await send_daily_papers_update(bot, settings)
         except Exception as e:
--- src/hf_daily_papers_tg/commands/error_message.py
+++ src/hf_daily_papers_tg/commands/error_message.py
@@ -8,12 +8,15 @@ from telegram.constants import ParseMode
 logger = logging.getLogger(__name__)
 
 
-async def send_error_message(bot: Bot, admin_user_id: int, exception: Exception) -> None:
+async def send_error_message(bot: Bot, admin_user_id: int, exception: Exception, message: str | None = None) -> None:
     """Send an error message to the admin user."""
     tb_list = traceback.format_exception(type(exception), exception, exception.__traceback__)
     tb_string = "".join(tb_list)
+    msg_str = ""
+    if message:
+        msg_str += f"\n<pre>{html.escape(message)}</pre>\n\n"
 
     logger.error(tb_string)
-    message = f"An exception was raised while handling an update\n<pre>{html.escape(tb_string)}</pre>"
+    message = f"An exception was raised while handling an update{msg_str}\n<pre>{html.escape(tb_string)}</pre>"
 
     await bot.send_message(chat_id=admin_user_id, text=message, parse_mode=ParseMode.HTML)
